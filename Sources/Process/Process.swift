c3d6561cd7547b8116e7ce5b33b4184e1b3f490d
Omar Abdelhafith <o.arrabi@me.com>
2017-02-03 21:43:52 +0000

    Making process public

--- Sources/Process/Process.swift
+++ Sources/Process/Process.swift
@@ -7,7 +7,7 @@
 //
 
 
-enum Process {
+public enum Process {
 
   /**
    Executes a command and captures its output
@@ -32,7 +32,7 @@ enum Process {
   public static func exec(_ command: String, settingsBlock: ((RunSettings) -> Void)) -> RunResults {
     let initalSettings = RunSettings()
     settingsBlock(initalSettings)
-    
+
     return exec(command, settings: initalSettings)
   }
 
@@ -63,19 +63,19 @@ enum Process {
   }
 
   public static func exec(_ command: String, settings: RunSettings) -> RunResults {
-        
+
     let result: RunResults
-    
+
     echoCommand(command, settings: settings)
-    
+
     if settings.dryRun {
       result = executeDryCommand(command)
     } else {
       result = executeActualCommand(command)
     }
-    
+
     echoResult(result, settings: settings)
-    
+
     return result
   }
 
@@ -83,35 +83,35 @@ enum Process {
     return execute(command, withExecutor: DryTaskExecutor())
   }
 
-  
+
   fileprivate static func executeActualCommand(_ command: String) -> RunResults {
     return execute(command, withExecutor: CommandExecutor.currentTaskExecutor)
   }
-  
+
   fileprivate static func execute(_ command: String, withExecutor executor: TaskExecutor) -> RunResults {
     let (status, stdoutPipe, stderrPipe) = executor.execute(command)!
-    
+
     let stdout = read(pipe: stdoutPipe)
     let stderr = read(pipe: stderrPipe)
     return RunResults(exitStatus: status, stdout: stdout, stderr: stderr)
   }
-  
+
   fileprivate static func echoCommand(_ command: String, settings: RunSettings) {
     if settings.echo.contains(.Command) {
       echoStringIfNotEmpty("Command", string: command)
     }
   }
-  
+
   fileprivate static func echoResult(_ result: RunResults, settings: RunSettings) {
     if settings.echo.contains(.Stdout) {
       echoStringIfNotEmpty("Stdout", string: result.stdout)
     }
-    
+
     if settings.echo.contains(.Stderr) {
       echoStringIfNotEmpty("Stderr", string: result.stderr)
     }
   }
-  
+
   fileprivate static func echoStringIfNotEmpty(_ title: String, string: String) {
     if !string.isEmpty {
       PromptSettings.print("\(title): \n\(string)")
